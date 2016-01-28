43587928585bc9a582b89348f9c45d80d11ffffb
mr-jing <877131516@qq.com>
2016-01-28 16:58:47 +0800

    添加 replies 表

---
+++ database/migrations/2016_01_28_163825_create_replies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->integer('topic_id')->index();
+            $table->text('original_content');
+            $table->text('content');
+            $table->integer('parent_id')->default(0);
+            $table->integer('like_count')->default(0)->index();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('replies');
+    }
+}
